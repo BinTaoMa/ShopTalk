2212cec052c76427a45e4c73e7d120cb35e24b73
BinTaoMa <2329660969@qq.com>
2023-05-23 20:53:35 +0800

    Revert "Initial commit"
    This reverts commit f4a2fec0ac2ab644464f83d47848d43254aeac2a.
    
    # Conflicts:
    #	.gitignore

--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
@@ -1,4 +1,4 @@
-package com.example.gulimallcoupon;
+package com.atguigu.gulimall.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- gulimall-product/src/test/java/com/example/gulimall/gulimallproduct/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/example/gulimall/gulimallproduct/GulimallProductApplicationTests.java
@@ -1,13 +1,37 @@
 package com.example.gulimall.gulimallproduct;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import com.atguigu.gulimall.product.GulimallProductApplication;
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
 
-@SpringBootTest
-class GulimallProductApplicationTests {
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = GulimallProductApplication.class)
+public class GulimallProductApplicationTests {
+
+    @Autowired(required = false)
+    public BrandService brandService;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+//        BrandEntity brandEntity = new BrandEntity();
+//        brandEntity.setName("华为");
+//        brandService.save(brandEntity);
+//        System.out.println("保存成功----");、
+        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
+        list.forEach((item)->{
+            System.out.println(item);
+        });
     }
 
 }
